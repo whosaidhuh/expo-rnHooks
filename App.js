2a12254fcc3b6173a9a235cb858438196b824f21
whosaidhuh <huh@whosaidhuh.me>
2021-01-11 21:29:39 -0500

    basic stack navigator using an index and route?

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper'
+import AppNavigator from './src/navigation'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <PaperProvider>
+        <AppNavigator />
+      </PaperProvider>
+  )
 }
 
 const styles = StyleSheet.create({
